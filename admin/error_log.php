a8cdba16d89425ecd8ee244109b559eb8f1f9b72
Moeini <a.moeny@gmail.com>
2025-08-23 19:45:09 +0330

    Dev: Reformat PHP Codes.

--- admin/error_log.php
+++ admin/error_log.php
@@ -3,12 +3,12 @@
 var_dump($ver);*/
 global $wpdb;
 $paging = '';
-$rowsa   = $wpdb->get_results( "select * from {$wpdb->prefix}didar_error order by id desc" );
+$rowsa  = $wpdb->get_results( "select * from {$wpdb->prefix}didar_error order by id desc" );
 
 //echo count($rowsa);
 
 
-$hpos_table = $wpdb->prefix . 'wc_orders';
+$hpos_table   = $wpdb->prefix . 'wc_orders';
 $hpos_enabled = $wpdb->get_var( $wpdb->prepare( "SHOW TABLES LIKE %s", $hpos_table ) ) === $hpos_table;
 
 if ( $hpos_enabled ) {
@@ -36,46 +36,59 @@ if ( $hpos_enabled ) {
 //echo count($rows);
 ?>
 <div class="wrap">
-	<h2><?php esc_attr_e('Error log', 'didar'); ?></h2>
-	<style>
-		table tr td pre{
-			max-height: 250px; /* Set maximum height */
-    		overflow-y: auto;  /* Enable vertical scrolling */
-    		max-width: 300px;
-			line-height: 1.5;
-		}
-	</style>
-	<form method="post" enctype="multipart/form-data">
-		<table class="widefat striped" style="max-width:100%;">
-			<thead>
-				<tr>
-					<th><?php esc_attr_e('Order ID', 'didar'); ?></th>
-					<th><?php esc_attr_e('Error date', 'didar'); ?></th>
-					<th><?php esc_attr_e('Error request', 'didar'); ?></th>
-					<th><?php esc_attr_e('Params', 'didar'); ?></th>
-					<th><?php esc_attr_e('Response', 'didar'); ?></th>
-				</tr>
-			</thead>
-			<body>
-				<?php
-				if(!empty($rows)){
-					$url = admin_url();
-					$paging = did_paging($rows,20);
-					foreach($rows as $row){
-						?><tr>
-							<td><a href="<?php echo admin_url("post.php?post=" . $row->order_id ."&action=edit"); ?>"><?php echo $row->order_id; ?></a></td>
-							<td><bdi><?php echo $row->date;?><bdi></td>
-							<td><bdi><?php echo $row->date;?><bdi></td>
-							<td><?php echo $row->path;?></td>
-							<td><pre dir="ltr"><?php print_r(json_decode($row->params,true));?><pre></td>
-							<td><pre dir="ltr"><?php print_r(json_decode($row->error));?><pre></td>
-						</tr>
-						<?php
-					}
-				}
-				?>
-			</body>
-		</table>
-		<?php echo $paging; ?>
-	</form>
+    <h2><?php esc_attr_e( 'Error log', 'didar' ); ?></h2>
+    <style>
+        table tr td pre {
+            max-height: 250px; /* Set maximum height */
+            overflow-y: auto; /* Enable vertical scrolling */
+            max-width: 300px;
+            line-height: 1.5;
+        }
+    </style>
+    <form method="post" enctype="multipart/form-data">
+        <table class="widefat striped" style="max-width:100%;">
+            <thead>
+            <tr>
+                <th><?php esc_attr_e( 'Order ID', 'didar' ); ?></th>
+                <th><?php esc_attr_e( 'Error date', 'didar' ); ?></th>
+                <th><?php esc_attr_e( 'Error request', 'didar' ); ?></th>
+                <th><?php esc_attr_e( 'Params', 'didar' ); ?></th>
+                <th><?php esc_attr_e( 'Response', 'didar' ); ?></th>
+            </tr>
+            </thead>
+            <body>
+            <?php
+            if ( ! empty( $rows ) ) {
+                $url    = admin_url();
+                $paging = did_paging( $rows, 20 );
+                foreach ( $rows as $row ) {
+                    ?>
+                    <tr>
+                        <td>
+                            <a href="<?php echo admin_url( "post.php?post=" . $row->order_id . "&action=edit" ); ?>"><?php echo $row->order_id; ?></a>
+                        </td>
+                        <td>
+                            <bdi><?php echo $row->date; ?>
+                                <bdi>
+                        </td>
+                        <td>
+                            <bdi><?php echo $row->date; ?>
+                                <bdi>
+                        </td>
+                        <td><?php echo $row->path; ?></td>
+                        <td>
+                        <pre dir="ltr"><?php print_r( json_decode( $row->params, true ) ); ?><pre>
+                        </td>
+                        <td>
+                        <pre dir="ltr"><?php print_r( json_decode( $row->error ) ); ?><pre>
+                        </td>
+                    </tr>
+                    <?php
+                }
+            }
+            ?>
+            </body>
+        </table>
+        <?php echo $paging; ?>
+    </form>
 </div>
--- inc/functions.php
+++ inc/functions.php
@@ -1,12 +1,13 @@
 <?php
 
 
-
-function did_get_sku($pid) {
-	if($sku = get_post_meta( $pid, "_sku", true))
+function did_get_sku( $pid ) {
+	if ( $sku = get_post_meta( $pid, "_sku", true ) ) {
 		return $sku;
+	}
 	$sku = "SKU_$pid";
-	update_post_meta($pid, "_sku",$sku);
+	update_post_meta( $pid, "_sku", $sku );
+
 	return $sku;
 	/*$product = wc_get_product($variation_id);
 
@@ -28,35 +29,41 @@ function did_get_sku($pid) {
     return '';*/
 }
 
-function add_errorlog($order_id,$path,$params,$output){
+function add_errorlog( $order_id, $path, $params, $output ) {
 	global $wpdb;
-	$path   = esc_sql($path);
-	$output = empty($output)?[]:$output;
-	$params = json_encode($params,JSON_UNESCAPED_UNICODE);
-	$output = json_encode($output,JSON_UNESCAPED_UNICODE);
+	$path   = esc_sql( $path );
+	$output = empty( $output ) ? [] : $output;
+	$params = json_encode( $params, JSON_UNESCAPED_UNICODE );
+	$output = json_encode( $output, JSON_UNESCAPED_UNICODE );
+
 	/*$out = $wpdb->get_var("select id from {$wpdb->prefix}didar_error where path='$path' and params='$params' and date>(NOW() - INTERVAL 5 MINUTE)");
 	if(!empty($out))
 		return $out;*/
-	return $wpdb->insert("{$wpdb->prefix}didar_error",[
-		'date'    =>date('Y-m-d H:i:s'),
-		'order_id'=>$order_id,
-		'error'   =>$output,
-		'path'    =>$path,
-		'params'  =>$params]);
+
+	return $wpdb->insert( "{$wpdb->prefix}didar_error", [
+		'date'     => date( 'Y-m-d H:i:s' ),
+		'order_id' => $order_id,
+		'error'    => $output,
+		'path'     => $path,
+		'params'   => $params
+	] );
 }
 
-function did_fix_price($price){
-	$opt = get_option( 'did_option', [] );
-	$price_type = isset($opt['price_type'])?$opt['price_type']:0;
-	if(get_option('woocommerce_currency')=='IRT' and 'IRR' == $price_type)
-		return $price*10;
-	if(get_option('woocommerce_currency')=='IRR' and 'IRT' == $price_type)
-		return $price/10;
+function did_fix_price( $price ) {
+	$opt        = get_option( 'did_option', [] );
+	$price_type = isset( $opt['price_type'] ) ? $opt['price_type'] : 0;
+	if ( get_option( 'woocommerce_currency' ) == 'IRT' and 'IRR' == $price_type ) {
+		return $price * 10;
+	}
+	if ( get_option( 'woocommerce_currency' ) == 'IRR' and 'IRT' == $price_type ) {
+		return $price / 10;
+	}
+
 	return $price;
 }
 
 
-function did_get_variation_id_by_sku($sku) {
+function did_get_variation_id_by_sku( $sku ) {
 	global $wpdb;
 
 	$variation_id = $wpdb->get_var(
